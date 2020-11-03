880d52ef51b9cf8c016550a8ab5ee37e18f42b54
Mohammad Rayya <mohamamdrayya1@hotmail.com>
2020-11-03 15:14:57 +0100

    l

--- js/script.js
+++ js/script.js
@@ -26,3 +26,40 @@ localStorage.setItem("ssss",this.getAttribute('data-coloer'));
 
 
 console.log(localStorage.getItem("ssss"));
+
+
+
+
+
+function Uppercase1(num)
+{
+
+var oldarray=num.split(' ');
+var newArray = [];
+
+
+
+for(var i=0;i<oldarray.length;i++)
+{
+
+newArray.push(oldarray[i].charAt(0).toUpperCase() + oldarray[i].slice(1));
+
+}
+
+
+return newArray.join(' ');
+
+}
+
+
+
+var bott=document.querySelector('#btn');
+var div1=document.querySelector("#result");
+var inputt=document.querySelector("#input1");
+
+bott.addEventListener('click',function()
+{
+div1.innerHTML=Uppercase1(inputt.value);
+
+
+})
